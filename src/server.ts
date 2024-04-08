6ab2a05990b0a8719c36721ce6c5fb6187eeffe1
Matheus Kuaye Muramoto <matheusmuramoto@gmail.com>
2024-04-07 22:23:04 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import fastify from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
+import { createEvent } from './routes/create-event';
+import { getAttendeeBadge } from './routes/get-attendee-badge';
+import { getEvent } from './routes/get-event';
+import { registerForEvent } from './routes/register-for-event';
+
+const app = fastify();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running');
+});
